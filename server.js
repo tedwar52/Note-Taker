958b083ca69e9c59b468e59b62858d69e951284c
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2020-11-05 16:41:03 -0500

    initial

---
+++ server.js
@@ -0,0 +1,57 @@
+//Imports=========================================
+
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+//var db = require("db.json");
+
+//=================================================
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//Data===============================================
+
+var Notes = [];
+//this might need to be moved to db.json
+
+//Routes=============================================
+
+//HTML ROUTES
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
+});
+
+//API ROUTES
+app.get("/api/notes", function (req, res) {
+    return res.json(Notes);
+});
+
+//create new note
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    Notes.push(newNote);
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    var currentNote = req.params.id;
+    console.log(currentNote);
+})
+
+//Start=Server========================================
+
+app.listen(PORT, function () {
+    console.log("it's running on PORT" + PORT);
+});
